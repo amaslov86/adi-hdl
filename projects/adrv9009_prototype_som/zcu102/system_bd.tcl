9662fdc861f482c5da5775c83a3ca047324dc511
Adrian Costina <adrian.costina@analog.com>
2018-05-10 12:15:10 +0300

    adrv9009_prototype_som: Improved throughput

--- projects/adrv9009_prototype_som/zcu102/system_bd.tcl
+++ projects/adrv9009_prototype_som/zcu102/system_bd.tcl
@@ -1,17 +1,24 @@
 
 set dac_fifo_name axi_adrv9009_dacfifo
-set dac_fifo_address_width 10
+set dac_fifo_address_width 16
 set dac_data_width 256
 set dac_dma_data_width 256
 
 source $ad_hdl_dir/projects/common/zcu102/zcu102_system_bd.tcl
 source $ad_hdl_dir/projects/common/xilinx/dacfifo_bd.tcl
+#source $ad_hdl_dir/projects/common/zcu102/zcu102_plddr4_dacfifo_bd.tcl
 
 ad_ip_parameter sys_ps8 CONFIG.PSU__FPGA_PL2_ENABLE 1
 ad_ip_parameter sys_ps8 CONFIG.PSU__CRL_APB__PL2_REF_CTRL__SRCSEL {IOPLL}
-ad_ip_parameter sys_ps8 CONFIG.PSU__CRL_APB__PL2_REF_CTRL__FREQMHZ 200
+ad_ip_parameter sys_ps8 CONFIG.PSU__CRL_APB__PL2_REF_CTRL__FREQMHZ 300
 
 source ../common/adrv9009_p_som_bd.tcl
+
+ad_ip_parameter axi_adrv9009_tx_dma CONFIG.DMA_DATA_WIDTH_SRC 128
+ad_ip_parameter axi_adrv9009_rx_dma CONFIG.DMA_DATA_WIDTH_DEST 128
+ad_ip_parameter axi_adrv9009_rx_os_dma CONFIG.DMA_DATA_WIDTH_DEST 128
+ad_ip_parameter axi_adrv9009_rx_dma CONFIG.FIFO_SIZE {16}
+ad_ip_parameter axi_adrv9009_rx_os_dma CONFIG.FIFO_SIZE {16}
 
 ad_connect sys_dma_clk sys_ps8/pl_clk2
 ad_connect sys_dma_rstgen/ext_reset_in sys_rstgen/peripheral_reset
