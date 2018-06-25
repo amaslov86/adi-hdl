88850c413f1565c5f2db4fa93cef6caa666ed03f
Istvan Csomortani <istvan.csomortani@analog.com>
2018-06-25 10:22:36 +0100

    adi_ip: Use ipx::associate_bus_interfaces command in clock interface definitions
    The main target is to simplify the implementation of the process, making
    shorter and more robust, by using predefine processes from ipx::.
    
     - First, the interface name should be the same as the port name. Xilinx
    IPs are using the same naming style, so this change will make things more
    consistent.
    
     - Second, Each clock signals should have two parameters: ASSOCIATED_BUSIF
    which defines the interface that is using the clock, and ASSOCIATED_RESET
    which define the clock's reset signal, if there is any.
    
    These parameters were defined and created manually. By using the
    ipx::associate_bus_interfaces command from IPI, the tool can create
    these parameters automatically.

--- library/scripts/adi_ip_xilinx.tcl
+++ library/scripts/adi_ip_xilinx.tcl
@@ -206,27 +206,28 @@ proc adi_add_multi_bus {num bus_name_prefix mode abs_type bus_type port_maps dep
 # \param[reset_signal_mode] - Reset mode (master/slave)
 #
 proc adi_add_bus_clock {clock_signal_name bus_inf_name {reset_signal_name ""} {reset_signal_mode "slave"}} {
-  set bus_inf_name_clean [string map {":" "_"} $bus_inf_name]
-  set clock_inf_name [format "%s%s" $bus_inf_name_clean "_signal_clock"]
-  set clock_inf [ipx::add_bus_interface $clock_inf_name [ipx::current_core]]
+  # set bus_inf_name_clean [string map {":" "_"} $bus_inf_name]
+  # set clock_inf_name [format "%s%s" $bus_inf_name_clean "_signal_clock"]
+
+  set clock_inf [ipx::add_bus_interface $clock_signal_name [ipx::current_core]]
   set_property abstraction_type_vlnv "xilinx.com:signal:clock_rtl:1.0" $clock_inf
   set_property bus_type_vlnv "xilinx.com:signal:clock:1.0" $clock_inf
-  set_property display_name $clock_inf_name $clock_inf
+  set_property display_name $clock_signal_name $clock_inf
   set clock_map [ipx::add_port_map "CLK" $clock_inf]
   set_property physical_name $clock_signal_name $clock_map
 
-  set assoc_busif [ipx::add_bus_parameter "ASSOCIATED_BUSIF" $clock_inf]
-  set_property value $bus_inf_name $assoc_busif
+  # set assoc_busif [ipx::add_bus_parameter "ASSOCIATED_BUSIF" $clock_inf]
+  # set_property value $bus_inf_name $assoc_busif
 
   if { $reset_signal_name != "" } {
-    set assoc_reset [ipx::add_bus_parameter "ASSOCIATED_RESET" $clock_inf]
-    set_property value $reset_signal_name $assoc_reset
+    # set assoc_reset [ipx::add_bus_parameter "ASSOCIATED_RESET" $clock_inf]
+    # set_property value $reset_signal_name $assoc_reset
 
-    set reset_inf_name [format "%s%s" $bus_inf_name_clean "_signal_reset"]
-    set reset_inf [ipx::add_bus_interface $reset_inf_name [ipx::current_core]]
+    #set reset_inf_name [format "%s%s" $bus_inf_name_clean "_signal_reset"]
+    set reset_inf [ipx::add_bus_interface $reset_signal_name [ipx::current_core]]
     set_property abstraction_type_vlnv "xilinx.com:signal:reset_rtl:1.0" $reset_inf
     set_property bus_type_vlnv "xilinx.com:signal:reset:1.0" $reset_inf
-    set_property display_name $reset_inf_name $reset_inf
+    set_property display_name $reset_signal_name $reset_inf
     set_property interface_mode $reset_signal_mode $reset_inf
     set reset_map [ipx::add_port_map "RST" $reset_inf]
     set_property physical_name $reset_signal_name $reset_map
@@ -238,6 +239,7 @@ proc adi_add_bus_clock {clock_signal_name bus_inf_name {reset_signal_name ""} {r
       set_property value "ACTIVE_HIGH" $reset_polarity
     }
   }
+  ipx::associate_bus_interfaces -busif $bus_inf_name -clock $clock_signal_name -reset $reset_signal_name [ipx::current_core]
 }
 
 ## Defines an IP as the current IP's dependency.
