fe24def7ab89c38a14732dae86b7248e15cf318a
Arpadi <sergiu.arpadi@analog.com>
2020-03-10 14:27:47 +0200

    adrv9364: Replaced axi_gpreg with axi_gpio
    just like the adrv9361 som, an fpga pin U13 is connected to a pulldown
    resistor R53 which is causing the loopback test to fail. U13 and V12
    were removed from the test

--- projects/adrv9364z7020/common/ccbob_bd.tcl
+++ projects/adrv9364z7020/common/ccbob_bd.tcl
@@ -6,14 +6,23 @@ ad_connect  sys_ps7/ENET1_GMII_TX_CLK GND
 
 # un-used io (regular)
 
-ad_ip_instance axi_gpreg axi_gpreg
-ad_ip_parameter axi_gpreg CONFIG.NUM_OF_CLK_MONS 0
-ad_ip_parameter axi_gpreg CONFIG.NUM_OF_IO 1
+ad_ip_instance axi_gpio gpio_0
+ad_ip_parameter gpio_0 CONFIG.C_IS_DUAL 1
+ad_ip_parameter gpio_0 CONFIG.C_GPIO_WIDTH 26
+ad_ip_parameter gpio_0 CONFIG.C_GPIO2_WIDTH 28
 
-create_bd_port -dir I -from 31 -to 0 gp_in_0
-create_bd_port -dir O -from 31 -to 0 gp_out_0
+create_bd_port -dir I -from 25 -to 0 gpio_0_c1_i
+create_bd_port -dir O -from 25 -to 0 gpio_0_c1_o
+create_bd_port -dir O -from 25 -to 0 gpio_0_c1_t
+create_bd_port -dir I -from 27 -to 0 gpio_0_c2_i
+create_bd_port -dir O -from 27 -to 0 gpio_0_c2_o
+create_bd_port -dir O -from 27 -to 0 gpio_0_c2_t
 
-ad_connect  gp_in_0 axi_gpreg/up_gp_in_0
-ad_connect  gp_out_0 axi_gpreg/up_gp_out_0
-ad_cpu_interconnect 0x41200000 axi_gpreg
+ad_connect gpio_0_c1_i gpio_0/gpio_io_i
+ad_connect gpio_0_c1_o gpio_0/gpio_io_o
+ad_connect gpio_0_c1_t gpio_0/gpio_io_t
+ad_connect gpio_0_c2_i gpio_0/gpio2_io_i
+ad_connect gpio_0_c2_o gpio_0/gpio2_io_o
+ad_connect gpio_0_c2_t gpio_0/gpio2_io_t
 
+ad_cpu_interconnect 0x41200000 gpio_0
