analogdevicesinc/hdl

sysid_intel: Added sysid to intel projects

projects/common/c5soc/c5soc_system_qsys.tcl
@@ -291,6 +291,20 @@ add_connection sys_clk.clk_reset sys_spi.reset
 add_interface sys_spi conduit end
 set_interface_property sys_spi EXPORT_OF sys_spi.external
 
+# system id
+
+add_instance axi_sysid_0 axi_sysid
+add_instance rom_sys_0 sysid_rom
+
+add_connection axi_sysid_0.if_rom_addr rom_sys_0.if_rom_addr
+add_connection rom_sys_0.if_rom_data axi_sysid_0.if_sys_rom_data
+add_connection sys_clk.clk rom_sys_0.if_clk
+add_connection sys_clk.clk axi_sysid_0.s_axi_clock
+add_connection sys_clk.clk_reset axi_sysid_0.s_axi_reset
+
+add_interface pr_rom_data_nc conduit end
+set_interface_property pr_rom_data_nc EXPORT_OF axi_sysid_0.if_pr_rom_data
+
 # interrupts
 
 ad_cpu_interrupt 0 sys_gpio_bd.irq
@@ -305,4 +319,4 @@ ad_cpu_interconnect 0x00010000 sys_id.control_slave
 ad_cpu_interconnect 0x00010080 sys_gpio_bd.s1
 ad_cpu_interconnect 0x00010100 sys_gpio_in.s1
 ad_cpu_interconnect 0x00109000 sys_gpio_out.s1
-
+ad_cpu_interconnect 0x00008000 axi_sysid_0.s_axi

amaslov86/adi-hdl

projects/common/c5soc/c5soc_system_qsys.tcl
@@ -291,6 +291,20 @@ add_connection sys_clk.clk_reset sys_spi.reset
 add_interface sys_spi conduit end
 set_interface_property sys_spi EXPORT_OF sys_spi.external
 
+# system id
+
+add_instance axi_sysid_0 axi_sysid
+add_instance rom_sys_0 sysid_rom
+
+add_connection axi_sysid_0.if_rom_addr rom_sys_0.if_rom_addr
+add_connection rom_sys_0.if_rom_data axi_sysid_0.if_sys_rom_data
+add_connection sys_clk.clk rom_sys_0.if_clk
+add_connection sys_clk.clk axi_sysid_0.s_axi_clock
+add_connection sys_clk.clk_reset axi_sysid_0.s_axi_reset
+
+add_interface pr_rom_data_nc conduit end
+set_interface_property pr_rom_data_nc EXPORT_OF axi_sysid_0.if_pr_rom_data
+
 # interrupts
 
 ad_cpu_interrupt 0 sys_gpio_bd.irq
@@ -305,4 +319,4 @@ ad_cpu_interconnect 0x00010000 sys_id.control_slave
 ad_cpu_interconnect 0x00010080 sys_gpio_bd.s1
 ad_cpu_interconnect 0x00010100 sys_gpio_in.s1
 ad_cpu_interconnect 0x00109000 sys_gpio_out.s1
-
+ad_cpu_interconnect 0x00018000 axi_sysid_0.s_axi